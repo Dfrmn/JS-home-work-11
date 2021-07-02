f56a8e58d4e29c80e86c1c237c856611cea5ed07
Dfrmn <danikfurman2010@gmail.com>
2021-07-02 17:56:25 +0300

    not correct

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+let num = +prompt(`Введите число`);
+
+function func(num){
+    for (let i = 2; i < Math.sqrt(num); i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+        return true;
+}
+alert(func());
